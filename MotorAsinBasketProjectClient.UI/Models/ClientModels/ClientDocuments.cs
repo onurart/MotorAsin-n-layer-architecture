f4752580f194f68fed5e886a9bbdca6a037dabff
onurart <onurumutluoglu@gmail.com>
2023-05-26 17:20:42 +0300

    EXCCELL JSON PDF eKLENDİ

---
+++ MotorAsinBasketProjectClient.UI/Models/ClientModels/ClientDocuments.cs
@@ -0,0 +1,14 @@
+﻿namespace MotorAsinBasketProjectClient.UI.Models.ClientModels
+{
+    public class ClientDocuments
+    {
+        public short? DocumetType { get; set; }
+        public short? LineType { get; set; }
+        public short? Billed { get; set; }
+        public double? TlToltal { get; set; }
+        public int? ProductReferance { get; set; }
+        public int? CustomerReferance { get; set; }
+        public string? DocumentNo { get; set; }
+        public DateTime? DocumentDate { get; set; }
+    }
+}
